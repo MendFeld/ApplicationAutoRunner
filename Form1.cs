eafe10359aa8bb2a4fd2ac24da670349c7d50aee
MendFeld <53190630+MendFeld@users.noreply.github.com>
2019-07-25 07:55:15 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace AutoRunStartup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFT\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            reg.SetValue("My application", Application.ExecutablePath.ToString());
+            MessageBox.Show("You have been successfully saved.", "Message", MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+    }
+}
